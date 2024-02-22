47446ea8d60429f5831115098892130246c20c66
SirSanctified <mambambopritchard@gmail.com>
2024-02-22 09:35:04 +0200

    Add yield_fixture test fixture

--- tests/test_ex2.py
+++ tests/test_ex2.py
@@ -11,6 +11,12 @@ def fixture_1():
     print("run fixture 1")
     return 2
 
+@pytest.fixture
+def yield_fixture():
+    print("Start the test phase")
+    yield 10
+    print("End the test phase")
+
 def test_with_fixture(fixture_1):
     print("run test example 1")
     num = fixture_1
@@ -21,3 +27,7 @@ def test_with_fixture2(fixture_1):
     print("run test example 2")
     num = fixture_1
     assert num == 2
+
+def test_with_yield_fixture(yield_fixture):
+    print("run test example 3")
+    assert yield_fixture == 10

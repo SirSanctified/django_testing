41eaa819e5ee85d79082cb3c02cf655c7fe95d8d
SirSanctified <mambambopritchard@gmail.com>
2024-02-22 09:42:32 +0200

    Add test for creating a user with Django database

---
+++ tests/test_db_user.py
@@ -0,0 +1,7 @@
+import pytest
+from django.contrib.auth.models import User
+
+@pytest.mark.django_db
+def test_user_create():
+    User.objects.create_user("test", "test@test.com", "test_user")
+    assert User.objects.count() == 1

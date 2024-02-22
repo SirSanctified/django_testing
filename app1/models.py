89bd9944d6b7eb5918796c2b26c0d030c899cceb
SirSanctified <mambambopritchard@gmail.com>
2024-02-22 12:45:42 +0200

    Add initial migration for Category and Product models, and remove category description field

--- app1/models.py
+++ app1/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return str(self.name)
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return str(self.name)
